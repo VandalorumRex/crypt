72882ae779e29356fb06879adbdd40bd2eb430e2
VandalorumRex <gho-mansur@yandex.ru>
2025-08-27 07:24:57 +0300

    init

---
+++ src/Service/EncodeService.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of EncodeService
+ *
+ * @author Mansur
+ */
+class EncodeService
+{
+
+    //put your code here
+}
